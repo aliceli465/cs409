25853032cb39a7ae455f6fb6a04df4b85b707b64
Alice Li <nameal1234@gmail.com>
2024-12-04 14:43:08 -0600

    export nehas component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
-
-
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   var na = "";
@@ -20,32 +18,33 @@ function App() {
   };
 
   if (showScreen) {
-  
     return (
       <div className="new-screen">
-        <h1 className = "greeting2">Past Uploads</h1>
-        <img src = "assets/icon.jpg" alt = "sm" className = "small-image2"/>
-        
-           <p className = "profileinfo">
-            Alice Li <br/ > </p>
-            <p className = "profileinfo2">
-            <strong>username:</strong> alice_li409 <br/ > 
-            <strong>email: </strong> alice_li409@illinois.edu</p> 
-            <div className="parent-container">
-            <div className="text-box-container">
-              <div className="text-box">file1.c</div> 
-        <div className="text-box">file2.c</div>
-        <div className="text-box">file3.c</div>
-        <div className="text-box">file4.c</div>
-      </div>
-      <div className="text-box-container2">
-        <div className="text-box2">Score: 1/10</div> 
-        <div className="text-box2">Score: 5/10</div>
-        <div className="text-box2">Score: 7/10</div>
-        <div className="text-box2">Score: 2/10</div>
-      </div>
-      </div>
-          
+        <h1 className="greeting2">Past Uploads</h1>
+        <img src="assets/icon.jpg" alt="sm" className="small-image2" />
+
+        <p className="profileinfo">
+          Alice Li <br />{" "}
+        </p>
+        <p className="profileinfo2">
+          <strong>username:</strong> alice_li409 <br />
+          <strong>email: </strong> alice_li409@illinois.edu
+        </p>
+        <div className="parent-container">
+          <div className="text-box-container">
+            <div className="text-box">file1.c</div>
+            <div className="text-box">file2.c</div>
+            <div className="text-box">file3.c</div>
+            <div className="text-box">file4.c</div>
+          </div>
+          <div className="text-box-container2">
+            <div className="text-box2">Score: 1/10</div>
+            <div className="text-box2">Score: 5/10</div>
+            <div className="text-box2">Score: 7/10</div>
+            <div className="text-box2">Score: 2/10</div>
+          </div>
+        </div>
+
         <button onClick={() => setShowScreen(false)}>Go Back</button>
       </div>
     );
@@ -61,14 +60,17 @@ function App() {
         onClick={handleImageClick}
       />
       <div className="center-content">
-
         <label className="upload-button">
           Upload Files
           <input type="file" onChange={handleFileUpload} />
         </label>
       </div>
       <div className="image-container">
-        <img src="/images/large-image.jpg" alt="Large" className="large-image" />
+        <img
+          src="/images/large-image.jpg"
+          alt="Large"
+          className="large-image"
+        />
       </div>
     </div>
   );
